3474d4312f17f9be55f3c5bbaadfd5d2eaca2889
liuweitao <liuweitao@daojia.com>
2019-02-14 14:21:22 +0800

    2

---
+++ src/main/java/com/daojia/authority/dto/BannerDto.java
@@ -0,0 +1,47 @@
+package com.daojia.authority.dto;
+
+import com.daojia.authority.service.CommonInterface;
+
+/**
+ * @Author: weitao.liu
+ * @Date: 2018/12/18 9:11
+ * @Description:
+ */
+public class BannerDto<T> implements CommonInterface {
+    private String bannerName;
+    private String location;
+    private double price;
+    private T data;
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getBannerName() {
+        return bannerName;
+    }
+
+    public void setBannerName(String bannerName) {
+        this.bannerName = bannerName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
---
+++ src/main/java/com/daojia/authority/thread/ClicBareisService.java
@@ -0,0 +1,28 @@
+package com.daojia.authority.thread;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * @Author: weitao.liu
+ * @Date: 2019/1/12 14:35
+ * @Description:
+ */
+public class ClicBareisService {
+
+    private CyclicBarrier cyclicBarrier;
+
+    public ClicBareisService(CyclicBarrier cyclicBarrier) {
+        this.cyclicBarrier = cyclicBarrier;
+    }
+
+    public void get() throws BrokenBarrierException, InterruptedException {
+        System.out.println("cyclicBarrier。。。。。"+Thread.currentThread().getName());
+        Thread.sleep(2000);
+        cyclicBarrier.await();
+        System.out.println("==============");
+        cyclicBarrier.await();
+        System.out.println("*******************");
+
+    }
+}
