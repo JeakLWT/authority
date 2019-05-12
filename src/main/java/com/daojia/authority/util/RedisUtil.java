ebeab491c4169e2ec10bc57d14a965b6f5759a95
lwt <LWT170210@163.com>
2019-05-12 17:27:28 +0800

    redis

---
+++ src/main/java/com/daojia/authority/util/RedisUtil.java
@@ -0,0 +1,26 @@
+package com.daojia.authority.util;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisShardInfo;
+
+public class RedisUtil {
+    private static final Logger logger = LogManager
+            .getLogger(RedisUtil.class);
+    private static Jedis jedis=null;
+
+    static {
+        logger.info("redis begin connecting.............");
+        JedisShardInfo jedisShardInfo=new JedisShardInfo("47.105.176.198",6379);
+        jedis = jedisShardInfo.createResource();
+        logger.info("redis begin connected.............");
+        jedis.ping();
+
+    }
+
+    public static String getKey(String key){
+       return jedis.get(key);
+    }
+
+}
